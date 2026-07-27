a2e48e22c1b66ad8a6902a07e9127ddd85b6e8d7
Luiz Solano <lsolanotelles@gmail.com>
2026-07-27 20:52:52 -0300

    feat: add ArrayList practice exercises

---
+++ src/week3/ArrayListMethod.java
@@ -0,0 +1,48 @@
+package week3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListMethod {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> productNames = new ArrayList<>();
+        int numberOption;
+        String productName;
+
+        System.out.println("Enter product name:");
+        productName = scan.nextLine();
+        addProduct(productNames, productName);
+
+        do {
+            System.out.println("Add another product? (1 - Yes / 0 - No)");
+            numberOption = scan.nextInt();
+            scan.nextLine();
+            switch (numberOption) {
+                case 1:
+                    System.out.println("Enter product name:");
+                    productName = scan.nextLine();
+                    addProduct(productNames, productName);
+                    break;
+                case 0:
+                    System.out.println("End");
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
+        } while (numberOption != 0);
+
+        listProducts(productNames);
+
+        scan.close();
+    }
+    static void addProduct(ArrayList<String> productNames, String productName) {
+        productNames.add(productName);
+    }
+    static void listProducts(ArrayList<String> productNames) {
+        for (int i = 0; i < productNames.size(); i++) {
+            System.out.println(productNames.get(i));
+        }
+    }
+}
